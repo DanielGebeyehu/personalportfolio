868b468ad2826dce1483ec891e8d4b5e2d820077
DanielG <danielgebeyehu45@gmail.com>
2025-10-29 09:11:43 -0700

    finaledit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,10 +5,10 @@ function About() {
     <section id="about" className="about">
       <h2>About Me</h2>
       <p>
-        Recent Electrical Engineering Graduate from University of Washington.
+        Recent Electrical Engineering Graduate from the University of Washington.
         Since graduation I have taken a course with Evangadi program for
         fullstack development to strengthen my knowledge on the software side
-        with in the intenstion of transitioning to machine learning.
+        with the intenstion of transitioning to machine learning.
       </p>
     </section>
   );
